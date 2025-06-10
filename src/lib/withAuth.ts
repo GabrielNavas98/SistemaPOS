81704271a53fb98578b06c3361f8a7a9e86a6a75
GabrielNavas98 <cgnavas98@gmail.com>
2025-06-10 09:12:29 -0300

    interface withAuth

--- src/lib/withAuth.ts
+++ src/lib/withAuth.ts
@@ -1,9 +1,15 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from "@/lib/authOptions"
 import { NextRequest, NextResponse } from 'next/server'
+interface paramsType {
+  params: { id: string }
+}
 
+interface sessionType {
+  user: { id: string }
+}
 export function withAuth(
-  handler: (req: NextRequest, ctx: { params: { id: string } }, session: { user: { id: string } }) => Promise<NextResponse>
+  handler: (req: NextRequest, ctx: paramsType, session: sessionType) => Promise<NextResponse>
 ) {
   return async function (req: NextRequest, ctx: { params: { id: string } }) {
     const session = await getServerSession(authOptions) as { user: { id: string } }
